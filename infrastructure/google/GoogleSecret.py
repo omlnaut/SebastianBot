f654526b7730cabfb9864e6c3305d199e36ee7b8
omlnaut <oneironaut.oml@gmail.com>
2025-10-18 20:17:44 +0000

    google tasks wip

--- infrastructure/google/GoogleSecret.py
+++ infrastructure/google/GoogleSecret.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+
+
+class Credentials(BaseModel):
+    token: str
+    refresh_token: str
+    token_uri: str
+    client_id: str
+    client_secret: str
+    scopes: List[str]
+    universe_domain: str
+    account: str
+    expiry: str
 
 
 class GoogleSecret(BaseModel):
-    credentials: str
+    credentials: Credentials
