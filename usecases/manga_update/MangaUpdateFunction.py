6a229177f2d9d88f0272a866cc3d1961e3d14050
omlnaut <oneironaut.oml@gmail.com>
2025-11-04 08:40:06 +0000

    finish manga update

--- usecases/manga_update/MangaUpdateFunction.py
+++ usecases/manga_update/MangaUpdateFunction.py
@@ -1,13 +1,18 @@
 import logging
-from datetime import datetime
-from pathlib import Path
 
 from azure.functions import EventGridOutputEvent, Out, TimerRequest
 
-from infrastructure.google.task.TaskAzureHelper import create_task_output_event
+from function_app import app
+from infrastructure.google.task.TaskAzureHelper import (
+    create_task_output_event,
+    task_output_binding,
+)
 from infrastructure.google.task.TaskModels import TaskListIds
 from infrastructure.google.task.TaskSchemas import CreateTaskEvent
-from infrastructure.telegram.AzureHelper import create_telegram_output_event
+from infrastructure.telegram.AzureHelper import (
+    create_telegram_output_event,
+    telegram_output_binding,
+)
 from shared.dates import is_at_most_one_day_old
 from usecases.manga_update.MangaModels import MangaPublisher, MangaUpdateManga
 from usecases.manga_update.MangaUpdateService import MangaUpdateService
@@ -18,10 +23,27 @@ mangas = [
         url="https://flamecomics.xyz/series/2",
         series_id=50369844984,
         publisher=MangaPublisher.FLAMECOMICS,
-    )
+    ),
+    MangaUpdateManga(
+        title="Auto Hunting with Clones",
+        url="https://flamecomics.xyz/series/109",
+        series_id=44327338345,
+        publisher=MangaPublisher.FLAMECOMICS,
+    ),
+    MangaUpdateManga(
+        title="One Piece",
+        url="https://mangaplus.shueisha.co.jp/titles/100020",
+        series_id=55099564912,
+        publisher=MangaPublisher.MANGAPLUS,
+    ),
 ]
 
 
+@app.timer_trigger(
+    schedule="7 3 * * *", arg_name="mytimer", run_on_startup=False, use_monitor=False
+)
+@task_output_binding()
+@telegram_output_binding()
 def check_manga_update(
     mytimer: TimerRequest,
     taskOutput: Out[EventGridOutputEvent],
@@ -32,33 +54,7 @@ def check_manga_update(
 
         service = MangaUpdateService()
 
-        tasks: list[EventGridOutputEvent] = []
-        for manga in mangas:
-            try:
-                latest_chapter = service.get_latest_chapter(manga)
-                logging.info(
-                    f"Latest chapter for {manga.title}: Chapter {latest_chapter.chapter} ({latest_chapter.release_date.strftime('%Y-%m-%d')})"
-                )
-
-                if is_at_most_one_day_old(latest_chapter.release_date):
-                    tasks.append(
-                        create_task_output_event(
-                            event=CreateTaskEvent(
-                                title=f"{manga.title} Chapter {latest_chapter.chapter}",
-                                notes=manga.url,
-                                task_list_id=TaskListIds.Mangas,
-                            )
-                        )
-                    )
-
-                    logging.info(
-                        f"Created task for {manga.title} Chapter {latest_chapter.chapter}"
-                    )
-
-            except Exception as e:
-                error_msg = f"Failed to process manga {manga.title}: {str(e)}"
-                logging.error(error_msg)
-                tasks.append(create_telegram_output_event(message=error_msg))
+        tasks = _check_all_manga(service)
 
         if tasks:
             taskOutput.set(tasks)  # type: ignore
@@ -66,3 +62,34 @@ def check_manga_update(
         error_msg = f"Error in manga_update: {str(e)}"
         logging.error(error_msg)
         telegramOutput.set(create_telegram_output_event(message=error_msg))
+
+
+def _check_all_manga(service) -> list[EventGridOutputEvent]:
+    tasks: list[EventGridOutputEvent] = []
+    for manga in mangas:
+        try:
+            latest_chapter = service.get_latest_chapter(manga)
+            logging.info(
+                f"Latest chapter for {manga.title}: Chapter {latest_chapter.chapter} ({latest_chapter.release_date.strftime('%Y-%m-%d')})"
+            )
+
+            if is_at_most_one_day_old(latest_chapter.release_date):
+                tasks.append(
+                    create_task_output_event(
+                        event=CreateTaskEvent(
+                            title=f"{manga.title} Chapter {latest_chapter.chapter}",
+                            notes=manga.url,
+                            task_list_id=TaskListIds.Mangas,
+                        )
+                    )
+                )
+
+                logging.info(
+                    f"Created task for {manga.title} Chapter {latest_chapter.chapter}"
+                )
+
+        except Exception as e:
+            error_msg = f"Failed to process manga {manga.title}: {str(e)}"
+            logging.error(error_msg)
+            tasks.append(create_telegram_output_event(message=error_msg))
+    return tasks
