64ae8faee7ba2a5b99a60178be8f61e8409a8570
omlnaut <oneironaut.oml@gmail.com>
2026-01-01 08:57:02 +0000

    log allhandler invocations

--- cloud/functions/infrastructure/AllHandler/function.py
+++ cloud/functions/infrastructure/AllHandler/function.py
@@ -3,8 +3,11 @@ import logging
 import uuid
 import azure.functions as func
 from cloud.functions.infrastructure.AllHandler.helper import allhandler_output_binding
+from cloud.functions.infrastructure.AllHandler.models import AllHandlerEvent
+from cloud.functions.infrastructure.google.task.models import CreateTaskEvent
 from cloud.functions.infrastructure.telegram.helper import telegram_output_binding
 from cloud.functions.infrastructure.telegram.models import SendTelegramMessageEvent
+from cloud.helper.parsing import parse_payload
 from function_app import app
 
 
@@ -15,16 +18,14 @@ def test_trigger_allhandler(
 ) -> func.HttpResponse:
     logging.info("HTTP test - emit trigger all handler event")
 
-    event = func.EventGridOutputEvent(
-        id=str(uuid.uuid4()),
-        data={"content": "Test AllHandler Trigger"},
-        subject="trigger_all_handler",
-        event_type="create_task_event",
-        event_time=datetime.now(),
-        data_version="1.0",
+    base_event = AllHandlerEvent(
+        create_task_events=[CreateTaskEvent(title="Test Task from AllHandler")],
+        send_telegram_message_events=[
+            SendTelegramMessageEvent(message="Test Message from AllHandler")
+        ],
     )
 
-    allHandlerOutput.set(event)
+    allHandlerOutput.set(base_event.to_output())
 
     return func.HttpResponse("emitted")
 
@@ -36,6 +37,24 @@ def all_handler(
     telegramOutput: func.Out[func.EventGridOutputEvent],
 ):
     logging.info("AllHandler triggered")
+
+    payload = parse_payload(azeventgrid, AllHandlerEvent)
+
+    # wip, just send telegram messages for now
+    # will get removed later
+    telegram_messages = []
+    for telegram_event in payload.send_telegram_message_events:
+        telegram_event.message += "\n\n(From AllHandler)"
+        telegram_messages.append(telegram_event.to_output())
+
+    for task_event in payload.create_task_events:
+        message = SendTelegramMessageEvent(
+            message=f"Task to be created: {task_event.title}: {task_event.notes or 'No notes'} at {task_event.due or 'No due date'}",
+        )
+        telegram_messages.append(message.to_output())
+
+    if telegram_messages:
+        telegramOutput.set(telegram_messages)  # type: ignore
 
     telegramOutput.set(
         SendTelegramMessageEvent(message="AllHandler triggered").to_output()
--- cloud/functions/infrastructure/AllHandler/models.py
+++ cloud/functions/infrastructure/AllHandler/models.py
@@ -1,9 +1,26 @@
-from pydantic import BaseModel
+from datetime import datetime
+from pydantic import BaseModel, Field
 
 from cloud.functions.infrastructure.google.task.models import CreateTaskEvent
 from cloud.functions.infrastructure.telegram.models import SendTelegramMessageEvent
 
+import azure.functions as func
+
 
 class AllHandlerEvent(BaseModel):
-    create_task_events: list[CreateTaskEvent]
-    send_telegram_message_events: list[SendTelegramMessageEvent]
+    create_task_events: list[CreateTaskEvent] = Field(
+        default_factory=list, description="List of Tasks to be created"
+    )
+    send_telegram_message_events: list[SendTelegramMessageEvent] = Field(
+        default_factory=list, description="List of Telegram messages to be sent"
+    )
+
+    def to_output(self) -> func.EventGridOutputEvent:
+        return func.EventGridOutputEvent(
+            id="allhandler-event",
+            data=self.model_dump(),
+            subject="allhandler_event",
+            event_type="allhandler_event",
+            event_time=datetime.now(),
+            data_version="1.0",
+        )
