e6363cab24b87367b4a8b28030e728c27137a8bb
omlnaut <oneironaut.oml@gmail.com>
2026-01-31 20:06:01 +0000

    remove opm+skeleton triggerTimes

--- cloud/functions/TriggerTimes.py
+++ cloud/functions/TriggerTimes.py
@@ -1,6 +1,4 @@
 class TriggerTimes:
-    OnePunchMan: str = "1 3 * * *"
-    SkeletonSoldier: str = "3 3 * * *"
     MangaUpdate: str = "5 3 * * *"
     DeliveryReady: str = "28 * * * *"  # Every hour at 28 minutes
     ReturnTracker: str = "35 * * * *"  # Every hour at 35 minutes
