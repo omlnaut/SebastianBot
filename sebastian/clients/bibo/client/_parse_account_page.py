1218498eb087f15da15027e90ec71b0b8835e6ba
omlnaut <oneironaut.oml@gmail.com>
2026-03-22 08:49:47 +0000

    cleanup bibo client parsing

--- sebastian/clients/bibo/client/_parse_account_page.py
+++ sebastian/clients/bibo/client/_parse_account_page.py
@@ -1,5 +1,6 @@
 # pyright: standard
 from datetime import datetime
+import re
 
 from bs4 import BeautifulSoup
 
@@ -10,27 +11,47 @@ def _parse_date(date_str: str) -> datetime:
     return datetime.strptime(date_str.strip(), "%d.%m.%Y")
 
 
+def _find_dates(row_content: str) -> TimeRange:
+    date_regex = re.compile(
+        r"(?P<from_date>\d{2}\.\d{2}\.\d{4}) - (?P<to_date>\d{2}\.\d{2}\.\d{4})"
+    )
+    match = date_regex.search(row_content)
+    if not match:
+        raise Exception(f"Did not find date information in {row_content}")
+
+    from_str = match.group("from_date")
+    to_str = match.group("to_date")
+
+    return TimeRange(
+        from_date=_parse_date(from_str),
+        to_date=_parse_date(to_str),
+    )
+
+
+def _find_location(date_and_location_cell_content):
+    location_regex = re.compile(r"(?P<location>.+), Tel. [\d]+")
+    match = location_regex.search(date_and_location_cell_content)
+    if not match:
+        raise Exception(
+            f"Did not find location information in {date_and_location_cell_content}"
+        )
+    location = match.group("location").strip()
+    return location
+
+
 def _parse_row(cells: list) -> BookLendingInfoRaw:
     book_id = cells[0].find("input", type="hidden")["value"]
     title = cells[1].find("strong").get_text(strip=True)
+    date_and_location_cell_content = cells[2].get_text(separator="\n", strip=True)
 
-    lines = [
-        line.strip()
-        for line in cells[2].get_text(separator="\n", strip=True).split("\n")
-        if line.strip()
-    ]
-    date_line = next(line for line in lines if " - " in line and line[0].isdigit())
-    from_str, to_str = date_line.split(" - ")
-    location = lines[-1].split("/")[-1].strip()
+    location = _find_location(date_and_location_cell_content)
+    lending_timerange = _find_dates(date_and_location_cell_content)
 
     return BookLendingInfoRaw(
         title=title,
         id=book_id,
         location=location,
-        lending_timerange=TimeRange(
-            from_date=_parse_date(from_str),
-            to_date=_parse_date(to_str),
-        ),
+        lending_timerange=lending_timerange,
     )
 
 
