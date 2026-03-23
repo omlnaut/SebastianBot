89e935ca7924d7dc07a40b1f30f612694e485bd3
omlnaut <oneironaut.oml@gmail.com>
2026-03-23 20:37:16 +0000

    CalendarEventClient: get calendars

--- investigations/google_token/create_credentials.py
+++ investigations/google_token/create_credentials.py
@@ -1,4 +1,6 @@
-# type: ignore
+# pyright: standard
+import json
+
 from google_auth_oauthlib.flow import InstalledAppFlow
 from pathlib import Path
 
@@ -7,6 +9,7 @@ SCOPES = [
     "https://www.googleapis.com/auth/gmail.modify",
     "https://www.googleapis.com/auth/tasks",
     "https://www.googleapis.com/auth/drive",
+    "https://www.googleapis.com/auth/calendar",
 ]
 
 
@@ -17,10 +20,12 @@ def main():
 
     flow = InstalledAppFlow.from_client_secrets_file(str(creds_path), SCOPES)
     creds = flow.run_local_server(port=0)
+    creds_json = json.loads(creds.to_json())
+    vault_dict = {"credentials": creds_json}
 
     # Save the credentials for the Cloud Function
     with open(current_dir / "logged_in_credentials.json", "w") as token:
-        token.write(creds.to_json())
+        token.write(json.dumps(vault_dict))
 
 
 if __name__ == "__main__":
