de4e6310f3599d3f1a8809ad1fbc20242e1c0747
omlnaut <oneironaut.oml@gmail.com>
2026-03-29 09:59:10 +0000

    cleanup usecase logic

--- sebastian/usecases/features/mietplan/handler.py
+++ sebastian/usecases/features/mietplan/handler.py
@@ -5,6 +5,7 @@ from datetime import datetime, timedelta
 from sebastian.protocols.google_drive import IGoogleDriveClient, UploadFileRequest
 from sebastian.protocols.mietplan import File, Folder, IMietplanClient
 from sebastian.protocols.models import AllActor, SendMessage
+from sebastian.shared.dates import is_within_timedelta
 from sebastian.usecases.usecase_handler import UseCaseHandler
 
 
@@ -34,16 +35,10 @@ class Handler(UseCaseHandler[Request]):
         """
         logging.info("Starting to process new mietplan files.")
 
-        newly_uploaded_files: list[str] = []
-        for file, folder in self._get_all_file_folder_pairs():
-            if not self._is_new_file(file, request.max_file_age):
-                continue
-
-            logging.info(f"  Found new file: {file.name}")
-            file_content = self._download_file(file)
-            upload_path = self._get_upload_path(file, folder)
-            self._upload_to_gdrive(upload_path, file_content)
-            newly_uploaded_files.append(upload_path)
+        newly_uploaded_files = [
+            self._process_new_file(file, folder)
+            for file, folder in self._get_all_new_files(request.max_file_age)
+        ]
 
         logging.info(
             f"Finished processing. Uploaded {len(newly_uploaded_files)} new files."
@@ -55,24 +50,26 @@ class Handler(UseCaseHandler[Request]):
         message = _create_message(newly_uploaded_files)
         return AllActor(create_tasks=[], send_messages=[SendMessage(message=message)])
 
-    def _get_all_file_folder_pairs(self) -> list[tuple[File, Folder]]:
+    def _process_new_file(self, file: File, folder: Folder) -> str:
+        logging.info(f"  Found new file: {file.name}")
+        file_content = self._download_file_from_mietplan(file)
+        upload_path = self._upload_to_gdrive(file, folder, file_content)
+        return upload_path
+
+    def _get_all_new_files(self, max_file_age: timedelta) -> list[tuple[File, Folder]]:
         return [
             (file, folder)
             for folder in self.mietplan_client.walk_from_top_folder()
             for file in folder.files
+            if is_within_timedelta(file.creation_date, max_file_age)
         ]
 
-    def _is_new_file(self, file: File, max_age: timedelta) -> bool:
-        return file.creation_date > datetime.now() - max_age
-
-    def _download_file(self, file: File) -> bytes:
+    def _download_file_from_mietplan(self, file: File) -> bytes:
         logging.info("    Downloading...")
         return self.mietplan_client.download_file(file.url)
 
-    def _get_upload_path(self, file: File, folder: Folder) -> str:
-        return f"{'/'.join(folder.path)}/{file.name}".strip("/")
-
-    def _upload_to_gdrive(self, upload_path: str, content: bytes):
+    def _upload_to_gdrive(self, file: File, folder: Folder, content: bytes) -> str:
+        upload_path = _build_upload_path(file, folder)
         upload_request = UploadFileRequest(
             filename=upload_path,
             content=content,
@@ -83,6 +80,7 @@ class Handler(UseCaseHandler[Request]):
         logging.info(
             f"    Uploaded to Google Drive with file_id: {response.file_id} at path: {upload_path}"
         )
+        return upload_path
 
 
 def _create_message(uploaded_files: list[str]) -> str:
@@ -90,3 +88,7 @@ def _create_message(uploaded_files: list[str]) -> str:
         [f"- {file}" for file in uploaded_files]
     )
     return message
+
+
+def _build_upload_path(file: File, folder: Folder) -> str:
+    return f"{'/'.join(folder.path)}/{file.name}".strip("/")
