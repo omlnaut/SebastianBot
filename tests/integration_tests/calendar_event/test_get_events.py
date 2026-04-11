035614ae76df21479584039123e2767c3437e231
omlnaut <oneironaut.oml@gmail.com>
2026-04-11 21:11:15 +0000

    finish integration tests

--- tests/integration_tests/calendar_event/test_get_events.py
+++ tests/integration_tests/calendar_event/test_get_events.py
@@ -15,3 +15,22 @@ def test_timerange():
 
     # Assert
     assert len(events) == 3
+
+
+def test_query():
+    # Arrange
+    client = resolve_calendar_event_client()
+
+    # Act
+    events = client.get_events(
+        Calendars.Primary,
+        q="1337",
+        time_min=date(2026, 1, 1),
+        time_max=date(2026, 1, 31),
+    )
+
+    # Assert
+    assert len(events) == 1
+    event = events[0]
+    assert event.description is not None
+    assert "1337" in event.description
