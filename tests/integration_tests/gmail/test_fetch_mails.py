93bbe44cec9733dd7dbcca2a3603cc449b5d7986
omlnaut <oneironaut.oml@gmail.com>
2026-05-15 19:32:40 +0000

    fix integration test

--- tests/integration_tests/gmail/test_fetch_mails.py
+++ tests/integration_tests/gmail/test_fetch_mails.py
@@ -15,13 +15,16 @@ def test_fetch_mails(gmail_client: GmailClient):
 
 
 def test_fetch_mails_has_read_state(gmail_client: GmailClient):
-    query = GmailQueryBuilder().on_date(datetime(2026, 5, 15)).build()
+    query = GmailQueryBuilder().on_date(datetime(2026, 1, 15)).build()
 
     emails = gmail_client.fetch_mails(query)
 
     assert len(emails) > 0
     assert all(isinstance(mail.is_read, bool) for mail in emails)
-    assert all(not mail.is_read for mail in emails)
+    for mail in emails:
+        assert (
+            mail.is_read is True
+        ), f"Expected email with subject {mail.snippet[:20]} to be read, but it was marked as unread."
 
 
 def test_query_builder_on_date(gmail_client: GmailClient):
